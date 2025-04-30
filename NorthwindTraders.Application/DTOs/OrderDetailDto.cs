ca432f4c8795980112ad6bdf715baae3e0e48fab
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2025-04-30 10:51:05 -0600

    feat: create the DTOs of the orders and its details

---
+++ NorthwindTraders.Application/DTOs/OrderDetailDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindTraders.Application.DTOs
+{
+    public class OrderDetailDto
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Unit price must be greater than 0")]
+        public decimal UnitPrice { get; set; }
+
+        [Required]
+        [Range(1, short.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+        public short Quantity { get; set; }
+
+        [Range(0, 1.0, ErrorMessage = "Discount must be between 0 and 1")]
+        public float Discount { get; set; }
+
+        public decimal TotalPrice => UnitPrice * Quantity * (1 - (decimal)Discount);
+    }
+}
---
+++ NorthwindTraders.Application/DTOs/OrderDto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindTraders.Application.DTOs
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int? EmployeeId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public int? ShipVia { get; set; }
+        public decimal Freight { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string ShipName { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string ShipAddress { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string ShipCity { get; set; }
+
+        [StringLength(15)]
+        public string ShipRegion { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string ShipPostalCode { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string ShipCountry { get; set; }
+        public List<OrderDetailDto> OrderDetail { get; set; } = new List<OrderDetailDto>();
+    }
+}
